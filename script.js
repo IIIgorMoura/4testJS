8c61fdfae32b7a79045e8e5509e158446ccadf01
IIIgorMoura <igor2007moura@gmail.com>
2023-08-15 15:33:38 -0300

    a

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let biblioteca=[];
+let biblioteca = [];
+
+
 
 function cadastro(nome, autor, ano) {
     var livro = {
@@ -7,16 +9,27 @@ function cadastro(nome, autor, ano) {
         ano: ano
     };
     biblioteca.push(livro)
-}
+};
 
 function adicionar() {
-    let nome=document.getElementById('nomeLivro').value;
-    let autor=document.getElementById('autorLivro').value;
-    let ano=document.getElementById('anoLivro').value;
+    let nome = document.getElementById('nomeLivro').value;
+    let autor = document.getElementById('autorLivro').value;
+    let ano = document.getElementById('anoLivro').value;
 
-    cadastro(nome, autor, ano)
-}
+    cadastro(nome, autor, ano);
+
+    let quantidade = biblioteca.length;
+    document.getElementById('result').innerHTML = quantidade + " livros cadastrados."
+};
 
 function mostrarLivros() {
-    console.log(`resultado:${biblioteca[0].nome}`);
-}
+    for (let x of biblioteca) {
+        console.log(x.nome, x.autor, x.ano)
+    };
+};
+
+function apagar() {
+    biblioteca.length = 0
+
+    document.getElementById('result').innerHTML = "Cadastre um livro"
+};
